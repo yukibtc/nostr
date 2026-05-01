ef08e01216e1842650718673f0ee115e69a8be91
Yuki Kishimoto <yukikishimoto@protonmail.com>
2026-05-01 15:21:59 +0200

    nostr: rework NIP-51 tags
    Signed-off-by: Yuki Kishimoto <yukikishimoto@protonmail.com>

--- crates/nostr/src/nips/nip51.rs
+++ crates/nostr/src/nips/nip51.rs
@@ -5,11 +5,129 @@
 //!
 //! <https://github.com/nostr-protocol/nips/blob/master/51.md>
 
-use alloc::string::String;
+use alloc::string::{String, ToString};
 use alloc::vec::Vec;
+use core::fmt;
 
 use super::nip01::Coordinate;
-use crate::{EventId, PublicKey, Tag, TagStandard, Url};
+use crate::event::tag::{Tag, TagCodec, TagStandard, impl_tag_codec_conversions};
+use crate::{EventId, PublicKey, Url, event, key};
+
+const WORD: &str = "word";
+const PUBLIC_KEY: &str = "p";
+const HASHTAG: &str = "t";
+const EVENT: &str = "e";
+
+/// NIP-51 error
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    /// Event error
+    Event(event::Error),
+    /// Key error
+    Key(key::Error),
+    /// Missing tag kind
+    MissingTagKind,
+    /// Missing public key
+    MissingPublicKey,
+    /// Missing hashtag
+    MissingHashtag,
+    /// Missing event ID
+    MissingEventId,
+    /// Missing word
+    MissingWord,
+    /// Unknown tag
+    UnknownTag,
+}
+
+impl core::error::Error for Error {}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Event(e) => e.fmt(f),
+            Self::Key(e) => e.fmt(f),
+            Self::MissingTagKind => f.write_str("Missing tag kind"),
+            Self::MissingPublicKey => f.write_str("Missing public key"),
+            Self::MissingHashtag => f.write_str("Missing hashtag"),
+            Self::MissingEventId => f.write_str("Missing event ID"),
+            Self::MissingWord => f.write_str("Missing word"),
+            Self::UnknownTag => f.write_str("Unknown tag"),
+        }
+    }
+}
+
+impl From<event::Error> for Error {
+    fn from(e: event::Error) -> Self {
+        Self::Event(e)
+    }
+}
+
+impl From<key::Error> for Error {
+    fn from(e: key::Error) -> Self {
+        Self::Key(e)
+    }
+}
+
+/// Standardized NIP-51 tags
+///
+/// <https://github.com/nostr-protocol/nips/blob/master/51.md>
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Nip51Tag {
+    /// `p` tag
+    PublicKey(PublicKey),
+    /// `t` tag
+    Hashtag(String),
+    /// `e` tag
+    Event(EventId),
+    /// `word` tag
+    Word(String),
+}
+
+impl TagCodec for Nip51Tag {
+    type Error = Error;
+
+    fn parse<I, S>(tag: I) -> Result<Self, Self::Error>
+    where
+        I: IntoIterator<Item = S>,
+        S: AsRef<str>,
+    {
+        let mut iter = tag.into_iter();
+        let kind: S = iter.next().ok_or(Error::MissingTagKind)?;
+
+        match kind.as_ref() {
+            PUBLIC_KEY => {
+                let public_key: S = iter.next().ok_or(Error::MissingPublicKey)?;
+                Ok(Self::PublicKey(PublicKey::from_hex(public_key.as_ref())?))
+            }
+            HASHTAG => {
+                let hashtag: S = iter.next().ok_or(Error::MissingHashtag)?;
+                Ok(Self::Hashtag(hashtag.as_ref().to_lowercase()))
+            }
+            EVENT => {
+                let event_id: S = iter.next().ok_or(Error::MissingEventId)?;
+                Ok(Self::Event(EventId::from_hex(event_id.as_ref())?))
+            }
+            WORD => {
+                let word: S = iter.next().ok_or(Error::MissingWord)?;
+                Ok(Self::Word(word.as_ref().to_string()))
+            }
+            _ => Err(Error::UnknownTag),
+        }
+    }
+
+    fn to_tag(&self) -> Tag {
+        match self {
+            Self::PublicKey(public_key) => {
+                Tag::new(vec![String::from(PUBLIC_KEY), public_key.to_hex()])
+            }
+            Self::Hashtag(hashtag) => Tag::new(vec![String::from(HASHTAG), hashtag.to_lowercase()]),
+            Self::Event(event_id) => Tag::new(vec![String::from(EVENT), event_id.to_hex()]),
+            Self::Word(word) => Tag::new(vec![String::from(WORD), word.clone()]),
+        }
+    }
+}
+
+impl_tag_codec_conversions!(Nip51Tag);
 
 /// Things the user doesn't want to see in their feeds
 #[derive(Debug, Clone, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -36,15 +154,15 @@ impl From<MuteList> for Vec<Tag> {
         let mut tags =
             Vec::with_capacity(public_keys.len() + hashtags.len() + event_ids.len() + words.len());
 
-        tags.extend(public_keys.into_iter().map(Tag::public_key));
-        tags.extend(hashtags.into_iter().map(Tag::hashtag));
-        tags.extend(event_ids.into_iter().map(Tag::event));
         tags.extend(
-            words
+            public_keys
                 .into_iter()
-                .map(TagStandard::Word)
-                .map(Tag::from_standardized),
+                .map(Nip51Tag::PublicKey)
+                .map(Into::into),
         );
+        tags.extend(hashtags.into_iter().map(Nip51Tag::Hashtag).map(Into::into));
+        tags.extend(event_ids.into_iter().map(Nip51Tag::Event).map(Into::into));
+        tags.extend(words.into_iter().map(Nip51Tag::Word).map(Into::into));
 
         tags
     }
@@ -57,10 +175,6 @@ pub struct Bookmarks {
     pub event_ids: Vec<EventId>,
     /// Coordinates
     pub coordinate: Vec<Coordinate>,
-    /// Hashtags
-    pub hashtags: Vec<String>,
-    /// Urls
-    pub urls: Vec<Url>,
 }
 
 impl From<Bookmarks> for Vec<Tag> {
@@ -68,21 +182,12 @@ impl From<Bookmarks> for Vec<Tag> {
         Bookmarks {
             event_ids,
             coordinate,
-            hashtags,
-            urls,
         }: Bookmarks,
     ) -> Self {
-        let mut tags =
-            Vec::with_capacity(event_ids.len() + coordinate.len() + hashtags.len() + urls.len());
+        let mut tags = Vec::with_capacity(event_ids.len() + coordinate.len());
 
         tags.extend(event_ids.into_iter().map(Tag::event));
         tags.extend(coordinate.into_iter().map(Tag::from));
-        tags.extend(hashtags.into_iter().map(Tag::hashtag));
-        tags.extend(
-            urls.into_iter()
-                .map(TagStandard::Url)
-                .map(Tag::from_standardized),
-        );
 
         tags
     }
@@ -159,5 +264,58 @@ impl From<ArticlesCuration> for Vec<Tag> {
         tags.extend(event_ids.into_iter().map(Tag::event));
 
         tags
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_nip51_public_key_tag() {
+        let public_key =
+            PublicKey::from_hex("04c915daefee38317fa734444acee390a8269fe5810b2241e5e6dd343dfbecc9")
+                .unwrap();
+        let tag = vec![
+            "p",
+            "04c915daefee38317fa734444acee390a8269fe5810b2241e5e6dd343dfbecc9",
+        ];
+        let parsed = Nip51Tag::parse(&tag).unwrap();
+
+        assert_eq!(parsed, Nip51Tag::PublicKey(public_key));
+        assert_eq!(parsed.to_tag(), Tag::parse(tag).unwrap());
+    }
+
+    #[test]
+    fn test_nip51_hashtag_tag() {
+        let tag = vec!["t", "Nostr"];
+        let parsed = Nip51Tag::parse(&tag).unwrap();
+
+        assert_eq!(parsed, Nip51Tag::Hashtag(String::from("nostr")));
+        assert_eq!(parsed.to_tag(), Tag::parse(["t", "nostr"]).unwrap());
+    }
+
+    #[test]
+    fn test_nip51_event_tag() {
+        let event_id =
+            EventId::from_hex("9ae37aa68f48645127299e9453eb5d908a0cbb6058ff340d528ed4d37c8994fb")
+                .unwrap();
+        let tag = vec![
+            "e",
+            "9ae37aa68f48645127299e9453eb5d908a0cbb6058ff340d528ed4d37c8994fb",
+        ];
+        let parsed = Nip51Tag::parse(&tag).unwrap();
+
+        assert_eq!(parsed, Nip51Tag::Event(event_id));
+        assert_eq!(parsed.to_tag(), Tag::parse(tag).unwrap());
+    }
+
+    #[test]
+    fn test_nip51_word_tag() {
+        let tag = vec!["word", "spam"];
+        let parsed = Nip51Tag::parse(&tag).unwrap();
+
+        assert_eq!(parsed, Nip51Tag::Word(String::from("spam")));
+        assert_eq!(parsed.to_tag(), Tag::parse(tag).unwrap());
     }
 }
